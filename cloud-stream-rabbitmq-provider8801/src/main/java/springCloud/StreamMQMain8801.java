959bbe719dc90c737f487bb217e0a1b0ca49adfe
Polarisronx <polarisronx@163.com>
2023-12-27 15:16:07 +0800

    update cloud-stream

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/springCloud/StreamMQMain8801.java
@@ -0,0 +1,19 @@
+package springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ * @Create 2023-12-27 15:01
+ * @Version 1.0
+ * ClassName StreamMQMain8801
+ * Package springCloud
+ * Description 模拟消息的发送者
+ */
+@SpringBootApplication
+public class StreamMQMain8801 {
+    public static void main (String[] args){
+        SpringApplication.run(StreamMQMain8801.class,args);
+    }
+}
