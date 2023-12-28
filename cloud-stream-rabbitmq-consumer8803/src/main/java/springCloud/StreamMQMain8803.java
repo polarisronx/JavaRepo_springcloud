e710c82599f6543c46c1f7084ed937edc17e4365
Polarisronx <polarisronx@163.com>
2023-12-28 17:24:11 +0800

    update cloud-stream:consumer

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springCloud/StreamMQMain8803.java
@@ -0,0 +1,19 @@
+package springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ * @Create 2023-12-27 18:49
+ * @Version 1.0
+ * ClassName StreamMQMain8802
+ * Package springCloud
+ * Description 消息的接受者
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main (String[] args){
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springCloud/service/ReceiverMessageListener.java
@@ -0,0 +1,28 @@
+package springCloud.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+/**
+ * @Author Administrator
+ * @Create 2023-12-27 18:51
+ * @Version 1.0
+ * ClassName ReceiverMessageListener
+ * Package springCloud.controller
+ * Description
+ */
+@Slf4j
+@EnableBinding(Sink.class) // 定义消息的推送管道，可以认为是消息收发的service,sink表示是消息接收端
+public class ReceiverMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        log.info("消费者2号，----->接收到的消息：{}",message.getPayload()+"  port: "+serverPort);
+    }
+}
